4641030b7967201b3d822aa45af6ae4619762720
Nel Ojeda <jmo84987@educastur.es>
2024-09-25 11:23:15 +0200

    First Demo

---
+++ MyLibrary/Customer.cs
@@ -0,0 +1,17 @@
+﻿namespace MyLibrary
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string FullName
+        {
+            get
+            { return Name + " " + Lastname; }
+            //return $"{Name} {LastName}";
+        }
+        //public string FullName => $"{Name} {Lastname}";
+    }
+}
---
+++ MyLibraryTest/OperationsNUnitTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    [TestFixture]
+    public class OperationsNUnitTest
+    {
+        [Test]
+        public void Add_ImputNumbers_RerturnNumber()
+        {
+            // 1. Arrange
+          Operations Operations = new Operations();
+            var Operation = new Operations();
+
+
+            // 2. Act
+            int result = Operations.Add(2, 5);
+        
+
+            // 3. Assert
+            Assert.That(result, Is.EqualTo(8));
+            
+        }
+
+        [Test]
+        public void IsEven_InputNumber_ReturnTrue()
+        {
+            //1. arrange
+            Operations operations = new Operations();
+            var Operation = new Operations();
+
+            //2. act
+            var result = operations.Add(2, 2);
+
+            //3. assert
+            Assert.That(result, Is
+                .True);
+        }
+        
+    }
+}
