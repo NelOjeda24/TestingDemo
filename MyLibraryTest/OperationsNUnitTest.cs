228a332bbdacb6a5f00e462c12f367faa87b28d6
Nel Ojeda <jmo84987@educastur.es>
2024-10-09 09:27:04 +0200

    7

--- MyLibraryTest/OperationsNUnitTest.cs
+++ MyLibraryTest/OperationsNUnitTest.cs
@@ -1,5 +1,5 @@
 ﻿using NUnit.Framework;
-using System;
+using System.Collections.Generic;
 
 namespace MyLibrary
 {
@@ -10,7 +10,7 @@ namespace MyLibrary
         public void Add_InputNumbers_ReturnSum()
         {
             // 1. Arrange
-            var operations = new Operations(); // Corrección del nombre y uso de la variable
+            var operations = new Operations();
 
             // 2. Act
             int result = operations.Add(2, 5);
@@ -52,10 +52,53 @@ namespace MyLibrary
             var operations = new Operations();
 
             // 2. Act
-            double result = operations.addDecimal(2.5, 3.2);
+            double result = operations.AddDecimal(2.5, 3.2); // Corrige el método a "AddDecimal"
 
             // 3. Assert
             Assert.That(result, Is.EqualTo(5.7).Within(0.01)); // Verifica la suma de decimales con tolerancia
+        }
+
+
+
+
+        //[Test]
+        //[TestCase(1, 10, new int[] { 2, 4, 6, 8, 10 })]
+        //[TestCase(0, 20, new int[] { 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 })]
+        //[TestCase(-10, 10, new int[] { -10, -8, -6, -4, -2, 0, 2, 4, 6, 8, 10 })]
+        //public void GetEvenNumbers_ReturnsCorrectEvenNumbers(int start, int end, int[] expected)
+        //{
+        //    // 1. Arrange
+        //    var operations = new Operations();
+
+        //    // 2. Act
+        //    IList<int> actual = operations.GetEvenNumbers(start, end);
+
+        //    // 3. Assert
+        //    Assert.That(actual, Is.EqualTo(expected)); // Verifica que los números pares devueltos son correctos
+        //}
+
+
+        [Test]
+        public void GetEvenNumbers_ImputRange_ReturnEvenNumbers()
+        {
+            // 1. Arrange
+            Operations operations = new();
+            int start = 0;
+            int end = 10;
+
+            // 2. Act
+            var result = operations.GetEvenNumbers(start, end);
+
+            // 3. Assert
+            Assert.That(result, Is.Not.Empty);
+            Assert.That(result, Is.Not.Null);
+            Assert.That(result[0], Is.TypeOf<int>());
+            Assert.That(result, Is.Ordered);
+            Assert.That(result, Does.Contain(4));
+            Assert.That(result, Is.Unique);
+            Assert.That(end - start > 0, Is.Not.Null);
+            Assert.That(end - start > 0, Is.True);
+            Assert.That
         }
     }
 }
